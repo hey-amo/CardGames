278de1d1f6b520303c7c46d9944d0eb9f1000316
AD <silentflute.ad@gmail.com>
2025-12-09 16:03:38 +0000

    Initial project build.  Remote UI tests

---
+++ Packages/SimpleCardGames/SimpleCardGames/SimpleCardGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCardGamesApp.swift
+//  SimpleCardGames
+//
+//  Created by Amarjit on 09/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCardGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
